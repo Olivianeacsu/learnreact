19ebf6add372153fdac483ede0d45e23fd3719a7
Olivia Neacsu <olivia.neacsu@nordnet.se>
2024-05-06 21:26:24 +0200

    Udemy course section 3 - var vs let declaration of variable

--- src/index.js
+++ src/index.js
@@ -6,3 +6,6 @@ let age = prompt("Guess  Ryan's age ..");
 // using template string
 let result = `Hello ${fname} ${lname}, you are ${age} years old.`;
 alert(result);
+
+// var is function scoped, which means a variable declared inside a function can be accessed only within that function.
+// On the other hand, let is block scoped, which means a variable declared in3 - var vside a block {} (like if, for loops) can only be accessed within that block.
