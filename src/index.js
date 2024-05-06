e78bc7810967e50d69ada83e7c2ad206c1bf48e2
Olivia Neacsu <olivia.neacsu@nordnet.se>
2024-05-06 21:49:32 +0200

    Udemy course section 5 - default values for parameters of function call

--- src/index.js
+++ src/index.js
@@ -9,3 +9,10 @@ alert(result);
 
 // var is function scoped, which means a variable declared inside a function can be accessed only within that function.
 // On the other hand, let is block scoped, which means a variable declared in3 - var vside a block {} (like if, for loops) can only be accessed within that block.
+
+function welcome(user = 'Mistery person', message = 'default greeting') {
+    alert(`Hello ${user}, ${message}`);
+}
+
+welcome(); // default values will be used
+welcome('Olivia', 'How are you?');
