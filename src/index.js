d40bed9cf007f5ad330eb2c5dec925335deb239f
Olivia Neacsu <olivia.neacsu@nordnet.se>
2024-05-06 21:25:04 +0200

    Udemy course section 6 - template strings

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ let fname = 'Ryan';
 let lname = 'D';
 let age = prompt("Guess  Ryan's age ..");
 
+
+// using template string
 let result = `Hello ${fname} ${lname}, you are ${age} years old.`;
 alert(result);
